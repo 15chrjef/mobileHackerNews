32b69dafcdf1734babcc2445799fb7c4c7c41bab
jeff christian <jchri@MacBook-Pro-2.loca>
2016-12-11 16:50:20 -0800

    try to fix story problem

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,17 +8,68 @@ import {
 } from 'react-native';
 const {height, width} = Dimensions.get('window');
 import Title from '../components/Title'
-
+import axios from 'axios'
 export default class HomeScreen extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      stories: ''
+    }
+  }
+   componentWillMount(){ 
+     let self = this;
+     fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+      .then((responseJson) => {
+        return ((JSON.parse(responseJson._bodyInit)).slice(0,10))
+      })
+      .then( async function(stories) {
+        let myStories = [];
+        for(var i = 0; i < stories.length; i++) {
+          await fetch(`https://hacker-news.firebaseio.com/v0/item/${stories[i]}.json?print=pretty`)
+          .then(async (response) => {
+            await myStories.push((response._bodyInit))
+          })
+        }
+          console.log('myStories', myStories)
+        self.setState({
+          stories: myStories
+        })  
+        console.log(self.state)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Title/>
-        <View style={styles.body}>
-          <Text>Body</Text>
+    if(this.state.stories !== ''){  
+      console.log('stories', this.state.stories)
+      return (
+        <View style={styles.container}>
+          <Title/>
+          <View style={styles.body}>
+            <View>
+              <Text style={styles.screenHeader}>Top Stories</Text>
+            </View>
+            <View>
+              <Text>
+                {this.state.stories}
+              </Text>
+            </View>
+          </View>
         </View>
-      </View>
-    );
+      );
+    } else {
+      return (
+           <View style={styles.container}>
+          <Title/>
+          <View style={styles.body}>
+            <View>
+              <Text style={styles.screenHeader}>Loading...</Text>
+            </View>
+          </View>
+        </View>
+      )
+    }
   }
 }
 
@@ -29,8 +80,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   body: {
-    justifyContent: 'center',
-    alignItems: 'center',
     flex: 10,
+  },
+  story: {
+    width: width,
+    height: 30
+  },
+  screenHeader: {
+    fontSize: 20
   }
 });
